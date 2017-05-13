5f4584a4efe8f621d154f3c604f4564340e15cb5
Rohini Kumar Barla <rohinikumar@icloud.com>
2017-05-13 12:50:00 +0530

    Added the problem statement

--- IBMPonderMay2017.c
+++ IBMPonderMay2017.c
@@ -1,6 +1,18 @@
-//
-// Problem: https://www.research.ibm.com/haifa/ponderthis/challenges/May2017.html
-//
+/*
+    Problem: https://www.research.ibm.com/haifa/ponderthis/challenges/May2017.html
+    Ponder This Challenge:
+    In the string CABACB, each letter appears exactly twice and the number of characters
+    between the "A"s is one, between the "B"s is two, and between the "C"s is three.
+    The same problem with the letters A-E cannot be solved, but adding a 
+    space (in the form of a hyphen) allows it to be solved.
+    For example, in the string ADAEC-DBCEB, the number of characters between the "A"s is one,
+    between the "B"s is two, between the "C"s is three (counting the space), between the "D"s
+    is four, and between the "E"s is five.
+    Find a string of letters (A-Z) and "-" whose length is no more than 53 characters, where
+    each of the 26 letters A-Z appears exactly twice, and there is one character between
+    the "A"s, two between the "B"s ... and 26 between the "Z"s.
+*/
+
 #include <stdio.h>
 
 #define SIZE 75
